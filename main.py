8f66d5854a994acfcd036c86173f4f6d18d40ce4
Sakshi04bari <sakshibari04@gmail.com>
2026-06-18 20:04:16 +0530

    Initial commit

--- main.py
+++ main.py
@@ -322,11 +322,13 @@ if __name__ == '__main__':
     # Configure Flask app settings
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
     
-    # Run the Flask app
+   
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
-app.run(
-    host="0.0.0.0",
-    port=port,
-    debug=False
-)
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
